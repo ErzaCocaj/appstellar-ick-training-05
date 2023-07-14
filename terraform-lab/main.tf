1d75e2e8c698f7eaa004337d57fed4fa502ef9a7
ErzaCocaj <erzacocaj@hotmail.com>
2023-07-14 20:52:16 +0200

    Adding S3 bucket

--- terraform-lab/main.tf
+++ terraform-lab/main.tf
@@ -5,6 +5,15 @@ data "aws_vpc" "this" {
 
 }
 
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket-erza"
+
+  tags = {
+    Name        = "Ick training"
+    Environment = "Dev"
+  }
+}
+
 output "vpc_id" {
   value       = data.aws_vpc.this.id
 
