5920164a263bb69de8756d2a6efa01a295499ce1
ErzaCocaj <erzacocaj@hotmail.com>
2023-07-21 20:28:21 +0200

    .

--- assignment-4-workspace/cloudfront.tf
+++ assignment-4-workspace/cloudfront.tf
@@ -10,7 +10,7 @@ resource "aws_cloudfront_origin_access_control" "web_client" {
 module "web_client_cdn" {
   source  = "terraform-aws-modules/cloudfront/aws"
   version = "3.2.1"
-  #aliases = [""]
+  aliases = ["erza.appstellar.training"]
 
   comment         = "CDN for web-client"
   enabled         = true
@@ -41,10 +41,10 @@ default_cache_behavior = {
   }
 
 
-  #   viewer_certificate = {
-  #     acm_certificate_arn = "arn:aws:acm:us-east-1:135367859851:certificate/1032b155-22da-4ae0-9f69-e206f825458b"
-  #     ssl_support_method  = "sni-only"
-  #   }
+    viewer_certificate = {
+      acm_certificate_arn = module.acm.acm_certificate_arn
+      ssl_support_method  = "sni-only"
+    }
 }
 
 
